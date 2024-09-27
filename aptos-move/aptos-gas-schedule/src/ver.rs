3d2870bebd16c09046df55f65165f40fbdd436b6
Igor <igor-aptos@users.noreply.github.com>
2024-09-27 08:45:02 -0700

    [move][stdlib] Implement mem::swap native move call

--- aptos-move/aptos-gas-schedule/src/ver.rs
+++ aptos-move/aptos-gas-schedule/src/ver.rs
@@ -88,5 +88,4 @@ pub mod gas_feature_versions {
     pub const RELEASE_V1_21: u64 = 25;
     pub const RELEASE_V1_22: u64 = 26;
     pub const RELEASE_V1_23: u64 = 27;
-    pub const RELEASE_V1_24: u64 = 28;
 }
